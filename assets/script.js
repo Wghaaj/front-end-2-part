009b853bdd9e9d99e4b4ac940dcacfcdb7aedb63
dianatatarenko160@gmail.com <dianatatarenko160@gmail.com>
2024-03-06 21:15:26 -0700

    finished

--- assets/script.js
+++ assets/script.js
@@ -2,6 +2,9 @@ $(".nav-link").click(function(e) {
     const eventTarget =  e.target;
     const targetSelector = $(eventTarget).data('target')
 
+    $('.nav-link').removeClass('active')
+    $(eventTarget).addClass('active');
+
     $('.tab-pane').removeClass("active");
     $('.tab-pane').addClass("fade");
     $(targetSelector).addClass("active");
