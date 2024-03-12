58a03e5cc3af6bf58a7568c926c5909d980738ae
dianatatarenko160@gmail.com <dianatatarenko160@gmail.com>
2024-03-12 20:31:55 +0000

    finished

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function smoking () {
     }
 
     let age = prompt("Скільки вам років?") 
-    while (isNaN(age) || age === "" || age <= 0) {
+    if (isNaN(age) || age === "" || age <= 0) {
         let age = prompt("Будь ласка, введіть коректний вік: ") 
     }
 
@@ -16,12 +16,13 @@ function smoking () {
     } else if (age < 18 && smoking) {
         alert(name + ", а твої батьки знають про це?")
     } else if(age >= 18 && smoking) {
-        alert("Що ж, " + name + "це ваш вибір. Але я не радила би курити")
+        alert("Що ж, " + name + " це ваш вибір. Але я не радила би курити")
     } else {
-        alert("Супер, " + name + "Мабуть ще й спортом займаєтесь!")
+        alert("Супер, " + name + "! Мабуть ще й спортом займаєтесь!")
     }
 }
 
 function no() {
     alert("Добре, це ваш вибір, дякую за приділений час")
 }
+
