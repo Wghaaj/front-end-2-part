37effa72598d10399be13192c4a8ea1f016f14d7
wghaaj <dianatatarenko160@gmail.com>
2024-03-23 22:31:05 +0000

    updated

--- js/script.js
+++ js/script.js
@@ -1,1 +1,29 @@
-console.log("it works");
+const sorted = movies.sort((a,b) => a - b)
+
+console.log(sorted.reverse(movies.date));
+
+const favourites = sorted.slice(0,16)
+
+console.log(favourites);
+
+const cardTitles = document.querySelectorAll(".title p")
+favourites.forEach((movies, index) => {
+    if (index < cardTitles.length) {
+        cardTitles[index].textContent = movies.title
+    }
+});
+
+const cardDescriptions = document.querySelectorAll(".description p")
+favourites.forEach((movies, index) => {
+    if (index < cardDescriptions.length) {
+        cardDescriptions[index].textContent = movies.extract
+    }
+})
+
+const cardImages = document.querySelectorAll(".card img")
+favourites.forEach((movies, index) => {
+    if (index < cardImages.length ) {
+        cardImages[index].src = movies.thumbnail
+        cardImages[index].alt = movies.title
+    }
+});
