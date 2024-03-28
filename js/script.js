21cc69554eb763364a5f4a0ea1585dbd17e8428e
wghaaj <dianatatarenko160@gmail.com>
2024-03-28 14:41:47 +0000

    updated

--- js/script.js
+++ js/script.js
@@ -6,39 +6,6 @@ const favourites = sorted.slice(0,16)
 
 // console.log(favourites);
 
-const cardTitles = document.querySelectorAll(".title p")
-newSorted.forEach((movies, index) => {
-    if (index < cardTitles.length) {
-        cardTitles[index].textContent = movies.title
-    }
-});
-
-const cardDescriptions = document.querySelectorAll(".description p")
-favourites.forEach((movies, index) => {
-    if (index < cardDescriptions.length) {
-        cardDescriptions[index].textContent = movies.extract
-    }
-})
-
-const cardImages = document.querySelectorAll(".card img")
-favourites.forEach((movies, index) => {
-    if (movies.thumbnail) {
-        const img = new Image ()
-        img.onload = function () {
-            cardImages[index].src = movies.thumbnail
-        }
-        img.onerror = function() {
-            cardImages[index].src = "images/coming-soon.jpg"
-        }
-        img.src = movies.thumbnail
-    } else {
-        cardImages[index].src = "images/coming-soon.jpg"
-    }
-
-    cardImages[index].alt = movies.title
-
-});
-
 const filtered = movies.filter(function(movie) {
     return movie.genres.includes("Action")
 })
@@ -55,3 +22,26 @@ const genres = movies.reduce(function (acc, movie) {
 }, [])
 
 console.log("Genres: ",genres);
+
+const wrapper = document.querySelector(".box-wrapper");
+
+favourites.forEach((movie) => {
+    const cardContainer  = document.createElement("div");
+    cardContainer.classList.add("card");
+
+    cardContainer.innerHTML = `<img src="${movie.thumbnail}" alt="${movie.title}">`;
+    
+    const cardTitle = document.createElement("div");
+    cardTitle.classList.add("title");
+    cardTitle.innerHTML = `<p>${movie.title}</p>`;
+
+    const cardDescriptions = document.createElement("div");
+    cardDescriptions.classList.add("description");
+    cardDescriptions.innerHTML = `<p class="cut-text">${movie.extract}</p>`;
+
+    cardContainer.append(cardTitle, cardDescriptions);
+    wrapper.append(cardContainer);
+});
+
+
+
