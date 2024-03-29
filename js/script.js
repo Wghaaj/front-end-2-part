b3b6eab7a6893270e3e22e6fa0765b99bb6161ab
wghaaj <dianatatarenko160@gmail.com>
2024-03-29 14:52:38 +0000

    finished

--- js/script.js
+++ js/script.js
@@ -17,7 +17,7 @@ const oldestLogged = logedUsers.reduce((acc, curr) => {
 });
 
 console.log("the latest logged user:",latestLogged);
-console.log("the oldest logged user", oldestLogged);
+console.log("the oldest logged user:", oldestLogged);
 
 //general_users
 
@@ -42,22 +42,41 @@ const oldest = generalUsers.reduce((acc, curr) => {
 
 const middleAge  = Math.floor((youngest.age + oldest.age) / 2 )
 
-// console.log(middleAge);
+console.log("middle age:", middleAge);
 
 let ageDifference = generalUsers.forEach(user => {
     user.ageDifference = Math.abs(user.age - middleAge)
 });
 
-console.log("difference:", ageDifference);
+// console.log("difference:", ageDifference);
 
 const sorted = generalUsers.sort((a,b) => a.ageDifference - b.ageDifference)
 
-console.log("sorted:", sorted);
+// console.log("sorted:", sorted);
 
 const closest = sorted.slice(0,16)
 
-console.log("closest:", closest);
+// console.log("closest:", closest);
 
+const wrapper = document.querySelector(".box-wrapper")
 
+closest.forEach(item => {
+    const cardContainer = document.createElement("div")
+    cardContainer.classList.add("card")
+
+    cardContainer.innerHTML = `<img src ="${item.image}" alt = "${item.username}">`
+
+    const cardInfo = document.createElement("div")
+    cardInfo.classList.add("info")
+    cardInfo.innerHTML = `<p>Name : ${item.firstName} ${item.lastName}</p>
+    <p>Age : ${item.age}</p>
+    <p>Address : ${item.address.address} </p>
+    <p>City : ${item.address.city} </p>
+    <p>Email : ${item.email} </p>`
+
+    cardContainer.append(cardInfo)
+    wrapper.append(cardContainer)
+
+})
 
 
