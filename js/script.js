e3bf0cff6295dad0200c6b7b6c72316829549865
wghaaj <dianatatarenko160@gmail.com>
2024-04-14 11:56:24 +0100

    started

---
+++ js/script.js
@@ -0,0 +1,13 @@
+console.log('It works!');
+
+const slider = new Slider('#mySlider', {
+  // nav: false,
+  // infinite: false,
+  autoplayTimeout: 1500,
+  pauseIfMouseOver: true,
+});
+
+
+const a = teacher.getGroup() // []
+
+console.log(teacher.getGroup());
